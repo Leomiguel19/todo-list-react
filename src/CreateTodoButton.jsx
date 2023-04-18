b70302c4f93455b75a3dfd9eb2b0defc8fbdda28
Leonardo Guilarte <leomiguel1907@gmail.com>
2023-04-18 08:51:06 -0400

    Event management

--- src/CreateTodoButton.jsx
+++ src/CreateTodoButton.jsx
@@ -2,8 +2,15 @@ import react from "react";
 import './CreateTodoButton.css';
 
 function CreateTodoButton(){
+  const onClickButton = (msg) => {
+    alert(msg);
+  }
+
   return(
-    <button className="CreateTodoButton">+</button>
+    <button 
+      className="CreateTodoButton"
+      onClick={() => onClickButton('Aqui se deberia abrir el modal')}
+    >+</button>
   );
 }
 
